a900b13f3de24f14f99fe575e869ae537825a7a4
David J. H. Shih <djh.shih@gmail.com>
2017-10-12 13:18:12 -0400

    Added script to dockerize.

---
+++ bin/dockerize.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+path=${root}/../pkgs/docker-images
+
+if [[ $# < 1 ]]; then
+  echo "usage: ${0##*/} [image]"
+  exit 1
+fi
+
+image=$1
+
+cd $path/$image
+
+echo "Building docker image..."
+nix-build ${image}.nix
+
+echo "Loading docker image..."
+docker -i result
+
+# commands to publish an image:
+
+# docker tag [image] [user]/[image]
+# docker push [user]/[image]
+
+cd -
