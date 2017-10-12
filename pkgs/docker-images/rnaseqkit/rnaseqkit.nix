b988489b835bbe1fd802284e0dcc886d9eb87b35
David J. H. Shih <djh.shih@gmail.com>
2017-10-12 13:17:16 -0400

    Build from core image.

--- pkgs/docker-images/rnaseqkit/rnaseqkit.nix
+++ pkgs/docker-images/rnaseqkit/rnaseqkit.nix
@@ -1,10 +1,11 @@
 let
   nixpkgs = import <nixpkgs> {};
-  
   biopkgs = import ../../../default.nix {};
+	coreImage = import ../core/core.nix;
 in
 nixpkgs.dockerTools.buildImage {
   name = "rnaseqkit";
+	fromImage = coreImage;
   contents = [
     biopkgs.star
     biopkgs.rsem
