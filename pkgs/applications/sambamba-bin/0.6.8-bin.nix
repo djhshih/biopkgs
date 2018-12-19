000d3e691180aa96d2ece88cd52b8bd40c39c5dc
David J. H. Shih <djh.shih@gmail.com>
2018-12-18 18:16:31 -0600

    Tried updating sambamba to 0.6.8 but build fails.
    Additionally, static binary uses .gz extension, which is not recognized by Nix...
    
    Therefore, keep default version at 0.6.6 for now.

---
+++ pkgs/applications/sambamba-bin/0.6.8-bin.nix
@@ -0,0 +1,27 @@
+{ stdenv, fetchurl }:
+
+stdenv.mkDerivation rec {
+  version = "0.6.8";
+  name = "sambamba-bin-${version}";
+
+  src = fetchurl {
+    url = "https://github.com/lomereiter/sambamba/releases/download/v${version}/sambamba-${version}-linux-static.gz";
+    sha256 = "1m0gp48p7v3ybgqywvjig8fggsyay3wc7vc0fkdnkshcq5ixwvp5";
+  };
+
+  # Work around the "unpacker appears to have produced no directories"
+  # case that happens when the archive doesn't have a subdirectory.
+  setSourceRoot = "sourceRoot=`pwd`";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp sambamba* $out/bin/sambamba
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Tools for working with SAM/BAM data";
+    license     = licenses.gpl2;
+    homepage    = "https://github.com/lomereiter/sambamba"; 
+    platforms   = platforms.unix;
+  };
+}
