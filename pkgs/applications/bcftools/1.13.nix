dfdf83de2941e4842c7217c7a0f37e22a9da75df
David J. H. Shih <djh.shih@gmail.com>
2021-09-29 23:22:18 -0500

    Updated bcftools.

---
+++ pkgs/applications/bcftools/1.13.nix
@@ -0,0 +1,38 @@
+{ stdenv, fetchurl, htslib, zlib, bzip2, lzma, curl, perl }:
+
+stdenv.mkDerivation rec {
+  version = "1.13";
+  name = "bcftools-${version}";
+
+  src = fetchurl {
+    url = "https://github.com/samtools/bcftools/releases/download/${version}/${name}.tar.bz2";
+    sha256 = "140bscypyivq1r7iwi8nfpqzxcxgnjh4fshra7xaipay5bda3gqk";
+  };
+
+  prePatch = ''
+    sed s/-lcblas/-lgslcblas/g -i Makefile
+  '';
+
+  buildInputs = [ htslib zlib bzip2 lzma curl perl ];
+
+  buildPhase = ''
+    make
+  '';
+
+  checkPhase = ''
+    make test
+  '';
+
+  installPhase = ''
+    make prefix=$out install
+    BCFTOOLS_PLUGINS=$out/lib/bcftools/
+    mkdir -p $BCFTOOLS_PLUGINS
+    install -D plugins/*.so $BCFTOOLS_PLUGINS
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Tools for reading/writing BCF2/VCF/gVCF files and calling/filtering/summarising SNP and short indel sequence variants";
+    license     = licenses.free;
+    homepage    = "http://www.htslib.org"; 
+  };
+}
